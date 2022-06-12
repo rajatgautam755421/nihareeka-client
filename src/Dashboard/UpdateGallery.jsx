92a8af95c46d01dcde0afdb25a3f40d65754c399
rajatgautam755421 <rg755421@gmail.com>
2022-06-12 08:54:35 +0545

    sdsdfsd

--- src/Dashboard/UpdateGallery.jsx
+++ src/Dashboard/UpdateGallery.jsx
@@ -239,6 +239,7 @@ const UpdateGallery = ({ setFetch, fetch1 }) => {
                       <button
                         className=" bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg contact__us__main__button mt-4"
                         onClick={handleClick}
+                        disabled={pic === "" ? true : false}
                       >
                         {picLoader ? (
                           <TailSpin
--- src/Home/News.jsx
+++ src/Home/News.jsx
@@ -1,10 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Backdrop from "@mui/material/Backdrop";
+import CloseIcon from "@mui/icons-material/Close";
 import "./News.css";
+import { Tooltip } from "@mui/material";
 
 const News = () => {
   const [events, setEvents] = useState([]);
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+
   const fetchDaata = async () => {
     const { data } = await axios.get("http://128.199.18.46:4003/api/v1/events");
     try {
@@ -18,6 +26,19 @@ const News = () => {
     fetchDaata();
   }, []);
 
+  const handleView = () => {
+    setShow(true);
+  };
+
+  const handleView2 = () => {
+    setShow1(true);
+  };
+  const handleView3 = () => {
+    setShow2(true);
+  };
+  const handleView4 = () => {
+    setShow3(true);
+  };
   return (
     <>
       <div className="max-w-screen-xl mx-auto relative p-5 sm:p-0 ">
@@ -26,31 +47,60 @@ const News = () => {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-12 gap-10">
           <div className="sm:col-span-12 lg:col-span-9">
-            <a href="#link">
-              <div
-                className=" bg-cover text-center overflow-hidden news__events__image__1"
-                style={{
-                  height: 500,
-                  backgroundImage: `url(${events[0] ? events[0].pic : null})`,
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                }}
-                title="Nihareeka News&Activities"
-              ></div>
-            </a>
+            <div
+              className=" bg-cover text-center overflow-hidden news__events__image__1"
+              style={{
+                height: 500,
+                backgroundImage: `url(${events[0] ? events[0].pic : null})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                cursor: "pointer",
+              }}
+              title="Nihareeka News&Activities"
+              onClick={handleView}
+            ></div>
+
             <div className="mt-3 text-center bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+              {show && (
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                  }}
+                  open={show}
+                  // onClick={handleClose}
+                >
+                  <img
+                    src={events[0] ? events[0].pic : null}
+                    alt=""
+                    srcset=""
+                    style={{
+                      objectFit: "contain",
+                      width: "90%",
+                      height: "90%",
+                    }}
+                  />{" "}
+                  <Tooltip title="close">
+                    <CloseIcon
+                      style={{
+                        position: "absolute",
+                        top: "0",
+                        width: "35px",
+                        height: "35px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setShow(false)}
+                    />
+                  </Tooltip>
+                </Backdrop>
+              )}
               <div className>
                 <a
                   href="#link"
                   className="block text-gray-900 font-bold text-2xl mb-2 hover:text-pink-600 transition duration-500 ease-in-out"
+                  onClick={handleView}
                 >
-                  <Link
-                    to="/news-event"
-                    style={{ color: "black" }}
-                    className="main__text__events"
-                  >
-                    {events[0] ? events[0].title : null}
-                  </Link>
+                  {events[0] ? events[0].title : null}
                 </a>
 
                 <p className="text-gray-700 text-base mt-2 mx-5 sm:mx-10">
@@ -61,52 +111,156 @@ const News = () => {
           </div>
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
-              <a href="#link">
-                <div
-                  className="h-40 bg-cover text-center overflow-hidden  news__events__image__1"
-                  style={{
-                    backgroundImage: `url(${events[1] ? events[1].pic : null})`,
+              {show1 && (
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
                   }}
-                  title="Nihareeka News&Activities"
-                ></div>
-              </a>
+                  open={show1}
+                >
+                  <img
+                    src={events[1] ? events[1].pic : null}
+                    alt=""
+                    srcset=""
+                    style={{
+                      objectFit: "contain",
+                      width: "90%",
+                      height: "90%",
+                    }}
+                  />{" "}
+                  <Tooltip title="close">
+                    <CloseIcon
+                      style={{
+                        position: "absolute",
+                        top: "0",
+                        width: "35px",
+                        height: "35px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setShow1(false)}
+                    />
+                  </Tooltip>
+                </Backdrop>
+              )}
+
+              <div
+                className="h-40 bg-cover text-center overflow-hidden  news__events__image__1"
+                style={{
+                  backgroundImage: `url(${events[1] ? events[1].pic : null})`,
+                  cursor: "pointer",
+                }}
+                title="Nihareeka News&Activities"
+                onClick={handleView2}
+              ></div>
+
               <Link
-                to="/news-event"
+                to="/"
                 className="main__text__events text-gray-900 inline-block font-semibold text-md my-2 hover:text-pink-600 transition duration-500 ease-in-out"
+                onClick={handleView2}
               >
                 {events[1] ? events[1].title : null}
               </Link>
             </div>
             <div className="mb-2">
-              <a href="#link">
-                <div
-                  className="h-40 bg-cover text-center overflow-hidden  news__events__image__1"
-                  style={{
-                    backgroundImage: `url(${events[2] ? events[2].pic : null})`,
+              {show2 && (
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
                   }}
-                  title="Nihareeka News&Activities"
-                ></div>
-              </a>
+                  open={show2}
+                >
+                  <img
+                    src={events[2] ? events[2].pic : null}
+                    alt=""
+                    srcset=""
+                    style={{
+                      objectFit: "contain",
+                      width: "90%",
+                      height: "90%",
+                    }}
+                  />{" "}
+                  <Tooltip title="close">
+                    <CloseIcon
+                      style={{
+                        position: "absolute",
+                        top: "0",
+                        width: "35px",
+                        height: "35px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setShow2(false)}
+                    />
+                  </Tooltip>
+                </Backdrop>
+              )}
+
+              <div
+                className="h-40 bg-cover text-center overflow-hidden  news__events__image__1"
+                style={{
+                  backgroundImage: `url(${events[2] ? events[2].pic : null})`,
+                  cursor: "pointer",
+                }}
+                title="Nihareeka News&Activities"
+                onClick={handleView3}
+              ></div>
+
               <Link
-                to="/news-event"
+                to="/"
                 className="main__text__events text-gray-900 inline-block font-semibold text-md my-2 hover:text-pink-600 transition duration-500 ease-in-out"
+                onClick={handleView3}
               >
                 {events[2] ? events[2].title : null}
               </Link>
             </div>
             <div>
-              <a href="#link">
-                <div
-                  className="h-40 bg-cover text-center overflow-hidden news__events__image__1"
-                  style={{
-                    backgroundImage: `url(${events[3] ? events[3].pic : null})`,
+              {show3 && (
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
                   }}
-                  title="Nihareeka News&Activities"
-                ></div>
-              </a>
+                  open={show3}
+                >
+                  <img
+                    src={events[3] ? events[3].pic : null}
+                    alt=""
+                    srcset=""
+                    style={{
+                      objectFit: "contain",
+                      width: "90%",
+                      height: "90%",
+                    }}
+                  />{" "}
+                  <Tooltip title="close">
+                    <CloseIcon
+                      style={{
+                        position: "absolute",
+                        top: "0",
+                        width: "35px",
+                        height: "35px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setShow3(false)}
+                    />
+                  </Tooltip>
+                </Backdrop>
+              )}
+              <div
+                className="h-40 bg-cover text-center overflow-hidden news__events__image__1"
+                style={{
+                  backgroundImage: `url(${events[3] ? events[3].pic : null})`,
+                  cursor: "pointer",
+                }}
+                title="Nihareeka News&Activities"
+                onClick={handleView4}
+              ></div>
+
               <Link
-                to="/news-event"
+                to="/"
                 className="main__text__events text-gray-900 inline-block font-semibold text-md my-2 hover:text-pink-600 transition duration-500 ease-in-out"
+                onClick={handleView4}
               >
                 {events[3] ? events[3].title : null}
               </Link>
