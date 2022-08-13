0a73719c0a49b6ee82dccd36edd70e810f3fa767
rajatgautam755421 <rg755421@gmail.com>
2022-08-13 15:45:08 +0545

    Navbar Ko Galti le garda feri push garera dimakh kharab bho

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = ({ clicked, setClicked }) => {
     setLoading(true);
     const getData = async () => {
       const { data } = await axios.get(
-        "http://128.199.18.46:4003/api/v1/notification"
+        "http://68.183.17.93:4003/api/v1/notification"
       );
       try {
         setLoading(true);
